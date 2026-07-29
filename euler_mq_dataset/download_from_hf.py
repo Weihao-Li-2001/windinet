91a431c8d6d5666c76fc503a867fa841c6830a53
Weihao Li <go76fuz@mytum.de>
2026-07-30 00:45:10 +0800

    resolve conflicts

--- euler_mq_dataset/download_from_hf.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import snapshot_download 
-
-snapshot_download(
-    repo_id="rha6696/euler_mq",
-    repo_type="dataset",
-    allow_patterns="128x128_ds/*",
-    local_dir="."
-)
